a3acc44350171d028507afe0949ff1b447164f36
sergio franco <sfranco@kolektor.com.ar>
2022-05-03 21:10:56 -0300

    se volvio a agregar boostrap

--- src/routes/verificaUsuario.js
+++ src/routes/verificaUsuario.js
@@ -27,11 +27,13 @@ router.post("/index",(req, res)=>{
               });
               const usuario = await rawResponse.json();
               //redirecciono a la pagina que quiero y envio los datos obtenidos
-              res.render("panelPrincipal"),{content:usuario, juegos:usuario.data.juegos};
+              console.log(usuario.data.juegos);
+              res.render("panelPrincipal", {content: usuario, juegos: usuario.data.juegos});
             })()
         }
+
         else{
-          w
+          console.log("usuario y contraseña incorrectos");
         }
       })();
 });
