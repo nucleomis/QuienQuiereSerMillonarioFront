6c74edb317d425a1b5885cb38ba570fe3859b7e8
guillermodck <81717640+guillermodck@users.noreply.github.com>
2022-05-10 21:15:06 -0300

    por favor comitealo

--- src/routes/preguntas.js
+++ src/routes/preguntas.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
-router.post("/nuevapregunta",(req,res)=>{
+router.post("/nuevoJuego",(req,res)=>{
   console.log("redireccionando a nueva pregunta");
-  res.render("preguntas");
+  res.render("nuevoJuego");
 })
 
 
