a8768a1f8c56ddf27a5bf98ff06857e563f07d33
sergio franco <sfranco@kolektor.com.ar>
2022-05-22 15:38:00 -0300

    cambios en participantes

--- src/routes/participante.js
+++ src/routes/participante.js
@@ -2,10 +2,27 @@ const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
 router.post("/participante",(req,res)=>{
-  console.log("redireccionando a Pantalla de Juego alumno");
-  res.render("participante");
-})
 
+  var id = req.body.id;
+  const url = "https://qqsm-api.herokuapp.com/juego/borrarJuego";
+  var cuerpo = {id:id};
 
+  (async () => {
+    const rawResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cuerpo)
+    });
+    const content = await rawResponse.json();
 
-module.exports = router;
+    req.session.juego = content.data;
+
+    console.log("juego: "+ content.data);
+
+    res.redirect("/juegoAlumno");
+})();
+});
+module.exports=router;
