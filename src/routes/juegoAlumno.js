2a979774496cab4ce44687cce1c1ebb4b5dea534
guillermodck <81717640+guillermodck@users.noreply.github.com>
2022-05-29 19:03:51 -0300

    navegacion preguntas

--- src/routes/juegoAlumno.js
+++ src/routes/juegoAlumno.js
@@ -4,11 +4,30 @@ const router = express.Router();
 
 router.post("/juegoAlumno",(req,res)=>{
   //envio el nombre del participante de la vista participante.hbs
-  req.session.nombreParticipante = req.body.nombreParticipante;
+
+  
+  var datosjuego = req.session.juegoiniciado.data;
+    console.log(req.session.nombreParticipante)
+  //console.log(pregunta);
+  //var res1 = req.session.juegoiniciado.preguntas.respuestas.respuesta;
+  
+    req.session.nombreParticipante = req.body.nombreParticipante; 
+    req.session.indicepregunta +=1;
+      res.render("juegoAlumno",{idJuego:req.session.idJuego, 
+      nombreParticipante:req.session.nombreParticipante, 
+      pregunta:datosjuego.preguntas[req.session.indicepregunta].pregunta,
+      res1:datosjuego.preguntas[req.session.indicepregunta -1].respuestas[0].respuesta,
+      res2:datosjuego.preguntas[req.session.indicepregunta -1].respuestas[1].respuesta,
+      res3:datosjuego.preguntas[req.session.indicepregunta -1].respuestas[2].respuesta,
+      res4:datosjuego.preguntas[req.session.indicepregunta -1].respuestas[3].respuesta,
+      
+      
+      
+    });
+    
   
   
-  console.log("redireccionando a Pantalla de Juego alumno");
-  res.render("juegoAlumno",{idJuego:req.session.idJuego, nombreParticipante:req.session.nombreParticipante});
-})
+
+});
 
 module.exports = router;
--- src/routes/participante.js
+++ src/routes/participante.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
-router.post("/participante",(req,res)=>{
+
+router.post("/jugando",(req,res)=>{
   
-  req.session.idJuego = req.body.id;
-  console.log("redireccionando a Pantalla del Juego participante con el id de juego: "+req.session.idJuego);
-  res.render("participante");
-})
+  var id=req.body.id;
+  var cuerpo = {id:22}
+  
+  const url = "https://qqsm-api.herokuapp.com/juego/iniciojuego";
+  (async () => {
+    const rawResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cuerpo)
+    });
+    
+  const content = await rawResponse.json();
+  
+  req.session.juegoiniciado = content;
+    req.session.indicepregunta = 0;
+  
+  //req.session.res1 = content.data.preguntas.respuestas.respuesta;
+  //req.session.res2 = content.data.preguntas.respuestas.respuesta;
+  //req.session.res3 = content.data.preguntas.respuestas.respuesta;
+  //req.session.res4 = content.data.preguntas.respuestas.respuesta;
+
+
+  //console.log(content)
+  res.render("participante")
+  
+  })();
+  });
+
 
 module.exports = router;
