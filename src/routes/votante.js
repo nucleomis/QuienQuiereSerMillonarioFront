ae2e1472177612cb681b508a098f83ef5dcc2e9f
guillermodck <81717640+guillermodck@users.noreply.github.com>
2022-06-01 17:15:57 -0300

    pantalla voto

--- src/routes/votante.js
+++ src/routes/votante.js
@@ -5,34 +5,13 @@ router.post("/votante",(req,res)=>{
   var id = req.body.id;
   req.session.idJuego = id;
   console.log("redireccionando a Pantalla de Votante ");
+  res.render("votantes1");
+});
+
+router.post("/pantallavotante",(req,res)=>{
+  console.log("redireccionando a Pantalla de Votante ");
   res.render("votante");
-})
-
-//formvotante.getElementById("idbtn").addEventListener("click", comodin);
-
-
-function comodin() {
-    formvotante = enabled
-    router.post("/participante", (req, res)=>{
-        const {conexion}=req.session.idJuego;
-            
-        if(conexion==1){
-            
-        }
-        if(conexion==2){
-            
-        }
-        if(conexion==3){
-            
-        }
-        else{
-           req.flash("alert_msg", "Agregue un ID de Juego");
-                   
-        }
-    
-        
-    })}
-
+});
 
 
 module.exports = router;
--- src/routes/votantes1.js
+++ src/routes/votantes1.js
@@ -2,33 +2,5 @@ const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
 
-router.post("/votantes1",(req, res)=>{
-  //res.render("index");//para renderizar y enviar el archivo con el nombre y la extencion previamente configurada como .hbs
-  const url = "https://qqsm-api.herokuapp.com/juego/iniciojuego"
-  console.log('ingresando a fetchito');
-
-(async () => {
-  const rawResponse = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  });
-console.log("llego aqui");
-const question = await rawResponse.json();
-
-console.log(question);
-//redirecciono a la pagina que quiero y envio los datos obtenidos
-
-req.session.res1 = usuario.data.res1;
-req.session.res2 = usuario.data.res2;
-req.session.res3 = usuario.data.res3;
-req.session.res4 = usuario.dato.res4;
-req.session.pista1 = usuario.data.pista1;
-req.session.pista2 = usuario.data.pista2;
-
-});
-})
 
 module.exports = router;
